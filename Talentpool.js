4f90bad6de934f9779d946a5ac9d7cbd7d61008b
apex apex <mosessamuel589@gmail.com>
2020-10-28 03:38:13 +0100

    landing page for talentpool

---
+++ Talentpool.js
@@ -0,0 +1,9 @@
+const close = document.querySelector('#close').addEventListener('click', closeMenu => {
+    document.querySelector('menu').style.display = "none"
+})
+
+ const menu = document.querySelector("#menu").addEventListener('click', onclick => {
+     document.querySelector('menu').style.display = "block"
+ })
+
+
