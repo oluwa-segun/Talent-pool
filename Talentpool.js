cb7e27716eee1d5de5309e2952a0551fafeec86c
apex apex <mosessamuel589@gmail.com>
2020-11-15 01:34:35 +0100

    first update

--- Talentpool.js
+++ Talentpool.js
@@ -8,122 +8,6 @@ const menu = document.querySelector("#menu").addEventListener('click', onclick =
 
 
 
-
-const slidetime = 500;
-const previousBtn = document.querySelector('.previous');
-const nextBtn = document.querySelector('.next')
-const allslides = [...document.querySelectorAll('.card')]
-
-
-
-const slider = document.querySelector('.test_cards')
-
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft
-
-})
-
-
-slider.addEventListener('mouseleave', (e) => {
-    isDown = false;
-    slider.classList.remove('active');
-})
-
-slider.addEventListener('mouseup', (e) => {
-    isDown = false;
-    slider.classList.remove('active');
-})
-
-
-slider.addEventListener('mousemove', (e) => {
-    if (!isDown)
-        return
-
-    e.preventDefault()
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk
-})
-
-nextBtn.addEventListener('click', (e) => {
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk
-})
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-
-const slidetime = 500;
-const previousBtn = document.querySelector('.previous');
-const nextBtn = document.querySelector('.next')
-const allslides = [...document.querySelectorAll('.card')]
-
-let clickable = true;
-let active = null;
-let newactive = null;
-
-
-function initSlider() {
-    allslides.forEach(slide => {
-        slide.setAttribute(
-            'style',
-            `transition: transform ${slidetime}ms ease;
-            animation-duration: ${slidetime}ms`
-        )
-    })
-}
-
-
-
-
-function changeSlide(forward) {
-    if (clickable) {
-        clickable = false;
-        active = document.querySelector('.active')
-        const activeSlideIndex = allslides.indexOf('active');
-
-if(forward){
-    console.log("active slide index: ", activeSlideIndex);
-    console.log("allslides length ", allslides.length);
-    console.log("new slide: ", (activeSlideIndex + 3) % allslides.length);
-
-    newactive = allslides[(activeSlideIndex + 1) % allslides.length]
-    active.classList.add('slideOutLeft');
-    newactive.classList.add('slideInRight', 'active')
-    }else{
-
-        }
-    }
-}
-
-nextBtn.addEventListener('click', () => {
-    changeSlide(true);
-
-})
-
-previousBtn.addEventListener(  'click', () => {
-    changeSlide(false);
-})
-
-
-
-initSlider();
-})
-
-
-
 function ScrollOne(){
     let cards = document.querySelectorAll('.tal-card');
     cards.forEach(card =>{
@@ -140,6 +24,39 @@ function ScrollOne(){
 window.addEventListener('scroll', ScrollOne)
 
 
+const slider = document.querySelector('.test_cards');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(x)
+  });
+
+console.log(isDown)
 
 
 
